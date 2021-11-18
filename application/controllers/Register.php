020f3d58688cafa0e6aa5bf3e3ae161fad56fd84
Allana <romero.allana2628@gmail.com>
2021-11-18 18:55:20 +0800

    reg and sql update

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -7,20 +7,51 @@
         {
             parent::__construct();
             
+            $this->load->library('form_validation');
+            #$this->load->library('encrypt');
             $this->load->model('Register_model');
         }
 
-        public function addUser()
+        public function validation()
+        {
+            $this->form_validation->set_rules('userName', 'Username', 'required|trim|min_length[3]|max_length[12]|is_unique[user.userName]',
+                array(
+                    'is_unique'     => 'This %s already exists.'
+                ));
+            $this->form_validation->set_rules('displayName', 'Display Name', 'required|trim');
+            $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]',
+                array(
+                    'is_unique'     => 'This %s already exists.'
+                ));
+            $this->form_validation->set_rules('password', 'Password', 'required');
+            $this->form_validation->set_rules('confirm_password', 'Confirm Password', 'required|matches[password]');
+
+            if($this->form_validation->run())
+            {
+                $verification_key = md5(rand());
+                $encrypted_password = md5($this->input->post('password'));
+                $this->addUser($verification_key, $encrypted_password);
+            }
+            else
+            {
+                $this->load->view('pages/registration');
+            }
+
+        }
+
+        public function addUser($verification_key, $encrypted_password)
         {          
             $var = $this->input->post();
 
             if(isset($var) && $var != null)
             {
-                $data['last_name'] = $this->input->post('last_name');
-                $data['first_name'] = $this->input->post('first_name');
-                $data['middle_name'] = $this->input->post('middle_name');
-                $data['email'] = $this->input->post('email');
-                $data['password'] = $this->input->post('password');
+                $data = array(
+                    'userName' => $this->input->post('userName'),
+                    'displayName' => $this->input->post('displayName'),
+                    'email' => $this->input->post('email'),
+                    'password' => $encrypted_password,
+                    'verification_Key' => $verification_key
+                );
 
                 $response = $this->Register_model->addNewUser($data);
                 if($response == true)
@@ -31,8 +62,6 @@
                 {
                     echo "Error";
                 }
-
-                
             }
             $this->load->view('pages/registration');
         }
--- application/views/pages/registration.php
+++ application/views/pages/registration.php
@@ -44,33 +44,44 @@
                     <h1 class="h1 mb-4 pt-4 text-dark">Registration</h1>
 
                     <p class="lead"> 
-                        <form method="post" action="<?= base_url() ?>Register/addUser">
+                        <form method="post" action="<?= base_url() ?>Register/validation" autocomplete="off">
                             <div class="mb-3">
-                              <label for="exampleInputlastname1" class="form-label">Last Name</label>
-                              <input type="text" class="form-control" id="examplelastname1" aria-describedby="emailHelp" name="last_name">
-                              <div class="mb-3">
-                             <label for="examplefirstname1" class="form-label">First Name</label>
-                             <input type="text" class="form-control" id="exampleInputlastname1" aria-describedby="emailHelp" name="first_name">
-                              <div class="mb-3">
-                             <label for="exampleInputmiddlename1" class="form-label">Middle Name (Optional)</label>
-                             <input type="text" class="form-control" id="exampleInputmiddlename1" aria-describedby="emailHelp" name="middle_name">
-                              <div class="mb-3">
-                             <label for="exampleInputEmail1" class="form-label">Email address</label>
-                             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
-                              <div class="mb-3">                      
+                              <label for="exampleInputlastname1" class="form-label">Username</label>
+                              <input type="text" class="form-control" id="examplelastname1" aria-describedby="emailHelp" name="userName"
+                                value="<?php echo set_value('userName'); ?>" />
+                                <span class="text-danger"><?php echo form_error('userName'); ?></span>
+
+                            <div class="mb-3">
+                              <label for="examplefirstname1" class="form-label">Display Name</label>
+                              <input type="text" class="form-control" id="exampleInputlastname1" aria-describedby="emailHelp" name="displayName"
+                                value="<?php echo set_value('displayName'); ?>" />
+                                <span class="text-danger"><?php echo form_error('displayName'); ?></span>
+                            
+                            <div class="mb-3">
+                              <label for="exampleInputEmail1" class="form-label">Email Address</label>
+                              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email"
+                                value="<?php echo set_value('email'); ?>" autocomplete="off" />
+                                <span class="text-danger"><?php echo form_error('email'); ?></span>                    
+                            
                             <div class="mb-3">
                               <label for="exampleInputPassword1" class="form-label">Password</label>
-                              <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+                              <input type="password" class="form-control" id="exampleInputPassword1" name="password"
+                                value="<?php echo set_value('password'); ?>" autocomplete="off" />
+                                <span class="text-danger"><?php echo form_error('password'); ?></span>
+
                             <div class="mb-3">
                               <label for="exampleInputconfirmpassword1" class="form-label">Confirm Password</label>
-                              <input type="password" class="form-control" id="exampleInputconfirmpassword1">
+                              <input type="password" class="form-control" id="exampleInputconfirmpassword1" name="confirm_password"
+                                value="<?php echo set_value('confirm_password'); ?>" />
+                                <span class="text-danger"><?php echo form_error('confirm_password'); ?></span>
+
                             </div>
                             <div class="mb-3">
                               
                               <label class="form-check-label" for="exampleCheck1"><p class="text-muted"> By signing up, you agree to the <a href="#" class="text-reset"> Terms of Service </a> and <a href="#" class="text-reset">Privacy Policy</a> </p></label>
                             </div>
 
-                            <button type="submit" name="signupbtn" style="background-color: #f0b63a;" class="btn-lg border border-3 border-dark"><a style="text-decoration: none; color: #212529;" href="<?php echo base_url(); ?>verification"> Register</a></button>
+                            <button type="submit" name="signupbtn" style="background-color: #f0b63a;" class="btn-lg border border-3 border-dark"><a style="text-decoration: none; color: #212529;"> Register</a></button>
 
                           </form>
                     </p>

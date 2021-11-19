0ac68061c3e7f2bb969a3d864d90c5334df0fd22
zek <zekguevarra1053@gmail.com>
2021-11-19 22:35:56 +0800

    mainpage virtual diage with bg options

--- application/views/pages/mainpage.php
+++ application/views/pages/mainpage.php
@@ -10,13 +10,13 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600&display=swap" rel="stylesheet"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <title>Main Page</title>
+    <title>Virtual Diary</title>
 </head>
 <body>
     <!--navbar section-->
     <nav class="navbar navbar-expand-lg navbar-light bg-dark ">
         <div class="container">
-            <a href="#" class="navbar-brand mb-0 h1 text-light px-5 mx-5 ">Logo</a>
+            <a href="#" class="navbar-brand mb-0 h1 text-light pe-5 me-5 "><img src="assets/images/vdLogo.png" height="40px"></a>
         <button type="button" 
         data-bs-toggle="collapse" 
         data-bs-target="#navbarNav" 
@@ -27,9 +27,9 @@
         <span class="navbar-toggler-icon">  </span>
     </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <div class="form d-flex px-5 mx-5">
+            <div class="form d-flex px-5 mx-5 col-sm-7">
                 <input type="text" class="form-control me-2">
-                <button type="submit" class="btn btn-outline btn-primary">Search</button>
+                <button type="submit" class="btn btn-outline btn-warning">Search</button>
             </div>
             <ul class="navbar-nav align-items-center navbar-right">
                 <li class="navbar-item text-light px-5 ms-5">
@@ -41,7 +41,7 @@
                     id="navbarDropdown" 
                     role="button" 
                     data-bs-toggle="dropdown">
-                    <i class="bi bi-person-circle h3" style="color: white;"></i>
+                    <i class="bi bi-person-circle h1" style="color: white;"></i>
                 </a>
                     <ul class="dropdown-menu " aria-labelledby="navbarDropdown">
                         <li><a href="#" class="dropdown-item ">Edit Profile</a></li>
@@ -53,6 +53,114 @@
         </div>
     </nav>
 <!-- end of navbar section -->
+<!-- background -->
+<div style="background-color: #ced4da;">
+<section class="projects">
+    <!-- <h1 class="display-3 text-dark fw-normal">Virtual Diary</h1>
+    <p class="lead">Pindutin para malaman ang hinaharap</p> -->
+    <div class="row pt-4">
+        <div class="project-col">
+            <img src="assets/images/baby.jpg" alt="project1">
+            <div class="layer">
+                <h3>rose <br> baby <br> blackpink</h3>
+            </div>
+        </div>
+        <div class="project-col">
+            <img src="assets/images/baby.jpg" alt="project2">
+            <div class="layer">
+                <h3>rose <br> baby <br> blackpink</h3>
+            </div>
+        </div>
+        <div class="project-col">
+            <img src="assets/images/baby.jpg" alt="project3">
+            <div class="layer">
+                <h3>rose <br> baby <br> blackpink</h3>
+            </div>
+        </div>
+    </div>
+</section>
+</div>
+<style>
+    .projects {
+  width: 80%;
+  margin: auto;
+  text-align: center;
+  padding-top: 50px;
+  height:100vh;
+}
+.project-col {
+  flex-basis: 32%;
+  border-radius: 10px;
+  margin-bottom: 30px;
+  position: relative;
+  overflow: hidden;
+}
+.project-col img {
+  width: 100%;
+}
+.layer {
+  background: transparent;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transition: 0.5s;
+}
+.layer:hover {
+  background: rgba(68, 104, 101, 0.7);
+}
+.layer h3 {
+  width: 100%;
+  font-weight: 600;
+  color: #edf0f1;
+  font-size: 20px;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  position: absolute;
+  opacity: 0;
+  transition: 0.5s;
+}
+.layer:hover h3 {
+  bottom: 49%;
+  opacity: 1;
+}
+/* footer section */
+.footer {
+  background: #111;
+  width: 100%;
+  text-align: center;
+  padding: 25px 0 10px 0;
+}
+.footer h4 {
+  margin-bottom: 20px;
+  margin-top: 15px;
+  font-weight: 600;
+  color: #edf0f1;
+}
+.footer fa {
+  margin: 0 13px;
+  cursor: pointer;
+  padding: 18px 0;
+}
+.footer i {
+  font-size: 20px;
+}
+.footerBot {
+  background: #000;
+  text-align: center;
+}
+.footerBot span {
+  color: #a1e8cc;
+}
+.footer i {
+  color: #edf0f1;
+}
+.icons a i:hover {
+  color: #a1e8cc;
+}
+</style>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>

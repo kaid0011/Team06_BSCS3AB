f3c418a2ad3512d0de7c878d185ff7b57b16e8a7
zek <zekguevarra1053@gmail.com>
2021-11-22 23:31:47 +0800

    revise v.2

--- application/views/pages/mainpage.php
+++ application/views/pages/mainpage.php
@@ -5,7 +5,7 @@
         <button type="button" 
         data-bs-toggle="collapse" 
         data-bs-target="#navbarNav" 
-        class="navbar-toggler" 
+        class="navbar-toggler custom-toggler" 
         aria-controls="navbarNav" 
         aria-expanded="false" 
         aria-label="Toggle navigation">
@@ -72,6 +72,13 @@
     flex-direction: column;
   }
 }
+.custom-toggler.navbar-toggler {
+            border-color: white;
+        } 
+        .custom-toggler .navbar-toggler-icon {
+            background-image: url(
+"data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255, 255, 255, 1)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
+        }
 .projects {
   width: 80%;
   margin: auto;

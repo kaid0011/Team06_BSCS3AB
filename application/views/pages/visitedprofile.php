bb082739af4d7df4fbe226f58c3573aad5899199
Jihad Yusoph <jihadyusoph.09@gmail.com>
2021-11-22 16:24:21 +0800

    Updated Visited Profile

--- application/views/pages/visitedprofile.php
+++ application/views/pages/visitedprofile.php
@@ -1,46 +1,19 @@
-<!--navbar main -->
- <nav class="navbar navbar-expand-lg navbar-light bg-dark ">
-        <div class="container">
-            <a href="#" class="navbar-brand mb-0 h1 text-light pe-5 me-5 "><img src="<?=base_url('assets/images/vdLogo.png')?>" height="40px"></a>
-        <button type="button" 
-        data-bs-toggle="collapse" 
-        data-bs-target="#navbarNav" 
-        class="navbar-toggler" 
-        aria-controls="navbarNav" 
-        aria-expanded="false" 
-        aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon">  </span>
-    </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <form action="<?= base_url() ?>Main/finduser" method ="post" class="form d-flex px-5 mx-5 col-sm-7"> <!--changes-->
-                <input type="text" name="userName" class="form-control me-2">
-                <button type="submit" class="btn btn-outline btn-warning">Search</button>
-            </form>                                                 <!--changes-->
-            <ul class="navbar-nav align-items-center navbar-right">
-                <li class="navbar-item text-light px-5 ms-5">
-                    <a href="#" class="nav-link text-light">Name <br><span class="text-secondary">Username</span> </a>
-                </li>
-                <li class="navbar-item dropdown">
-                    <a href="#" 
-                    class="nav-link dropdown-toggle text-white" 
-                    id="navbarDropdown" 
-                    role="button" 
-                    data-bs-toggle="dropdown">
-                    <i class="bi bi-person-circle h1" style="color: white;"></i>
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a href="#" class="dropdown-item ">Edit Profile</a></li>
-                        <li><a href="#" class="dropdown-item ">Logout</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-        </div>
-    </nav>
-<!--navbar main --> 
-  
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <link rel="icon" type="image/png" href="./favicon.png">
+    <title>Visited Profile</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
+  </head>
   <body style="background-color: #e9ecef;">
     <header>
+      <!--NAVBAR-->
       <nav class="navbar navbar-expand-lg  "style="background-color:#e9ecef;">
         <div class="container-fluid">
           
@@ -80,12 +53,13 @@
         </div>
 
       </nav>
-   
+   <!--END OF NAV BAR-->
     </header>
- 
+    <!--IMAGE USER-->
+    <main >
     <section style="background-color: #e9ecef;">
 <div class="container">
-   <!--div
+   <!--(OPTIONAL BACKGROUND IMAGE) div
   class="p-5 text-center bg-image"
   style="
   background-image: url('imgs/white.png');
@@ -93,223 +67,140 @@
 width: auto;"
    -->
 <div>
-  <?php
-  if($findUser->num_rows() > 0)
-  {
-    ?>
-      <div class="d-flex justify-content-center align-items-center h-100 ms-5 me-5">
-        <div class="text-white">
-          <div class="d-flex">
-            <a href="#"><img src="<?=base_url('assets/images/visitedprofile/profile.png')?>" class="rounded-circle ms-5 shadow-lg" alt="..." ></a>
-          </div>
-    <?php
-            foreach ($findUser->result() as $row)  
-            {  
-                ?>
-                <h1 class="lead text-center text-dark mt-1 ms-5 mb-5 fw-normal"><?php echo $row->displayName;?><br>
-                <a class="text-secondary  " style="text-decoration: none;">@<?php echo $row->userName;?></a>
-                </h1>
-                <?php
-            }
-    ?>
-        </div>
-      </div>
-
-      
-<div name="userFound" class="container" >
-<div class="row g-4 ">
-
-  <div class="col-md-6 col-lg-7 mt-5 " >
-    <div class="card bg-light " style="min-height: 400px;">
-      
-      <nav class="navbar navbar-light justify-content-center sticky-top" style="background-color: #495057;"> <p class=" justify-content-center align-items-center mt-2" style=" color: #eee;"> Public Notebook</p> </nav>
-      <div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-offset="0" class="scrollspy-example" tabindex="0">
-      <div class="card-body text-center" style="height: min-700px;">
-
-        <div>
-          <div class="card border-dark mb-3 " style="max-width: 50rem;">
-            <nav class="navbar navbar-light"> <ul class="nav nav-pills ms-2"  >
-              <li class="nav-item  ">
-                <p class="nav-link " href="#" style="color:black;">Date</p>
-              </li> </nav>
-            <div class="card-body text-dark">
-              
-              <p class="card-text1">Some quick example text to build on the card title and make up the bulk of the card's content .Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <div class="card-footer bg-transparent border-dark">
-              
-              <div>
-            <button class="btn btn-none btn-sm float-start"> <i class="bi bi-star h4"></i></button>
-                <button class="btn btn-secondary btn-sm float-end mt-1" type="button">Report</button>
-              </div> </div>
-          </div>   
-          <div class="card border-dark mb-3 " style="max-width: 50rem;">
-            
-            <nav class="navbar navbar-light"> <ul class="nav nav-pills ms-2"  >
-              <li class="nav-item  ">
-                <p class="nav-link " aria-current="page" href="#" style="color: black;">Date</p>
-              </li> </nav>
-            <div class="card-body text-dark">
-             
-              <p class="card-text1">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <div class="card-footer bg-transparent border-dark"> <div>
-             <button class="btn btn-none btn-sm  float-start"><i class="bi bi-star h4"></i></button>
-             <button class="btn btn-secondary btn-sm  float-end mt-1" type="button">Report</button>
-               </div></div>
-          </div>   
-                  
-                       
-      </div>
-      </div>
-      </div>
-    </div>
-  </div> 
-   <div class="col-md-6 col-lg-5 mt-5" style="border-radius: 50%;">
-    <div class="card bg-light" style="min-height: 400px;">
-      
-      <nav class="navbar navbar-light justify-content-center sticky-top" style="background-color: #495057;"> <p class=" justify-content-center align-items-center mt-2" style=" color: #eee;"> Discussion</p> </nav>
-      <div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-offset="0" class="scrollspy-example" tabindex="0">
-      <div class="card-body text-center" style="height: min-800px;">
-       
-        <div>
-          <div class="card border-dark mb-3 " style="max-width: 50rem;">
-            
-            <div class="card-body text-dark">
-              <div class="received-chats">
-                <div class="received-chats-img ">
-                    <img src="<?=base_url('assets/images/visitedprofile/profile.png')?>" style="display: inline-block;">
-
-                </div>
-                <div class="received-msg " style="   display: inline-block;
-                padding:0 0 0 10px;
-                vertical-align: top;
-                width: 92%;
-            ">
-                    <div class="received-mgs-inbox" style="background: #ced4da none repeat scroll 0 0;  border-radius: 10px; color: #646464; font-size: 14px; margin:0; padding: 5px 10px 5px 12px; width: 70%;">
-                        <p>
-                    HALOOOOOOOOOOOO
-                        </p>
-                       
-                        
-                    </div>
-
-                </div>
-                <div class="outgoing-chats" style="overflow: hidden; margin: 26px 20px; margin-left: 5px;">
-              
-                  <div class="outgoing-chat-msg"style="   width: 57%; float:left; margin-left:33%;">
-                      <div class="outgoing-mgs-inbox" style="background: #ced4da none repeat scroll 0 0;  border-radius: 10px; color: #646464; font-size: 14px; margin:0; padding: 5px 10px 5px 12px; width: 100%;">
-                          <p>
-                      HALOOOOOOOOOOOO
-                          </p>
-                          </div>
-                          </div>
-                          <div class="outgoing-chats-img " style="display: inline-block;">
-                            <img src="<?=base_url('assets/images/visitedprofile/profile.png')?>" style="display: inline-block;">
-                            </div>
-            </div>
-            </div>
-            
-             
-            </div>
-            
-            <div class="card-footer bg-transparent border-dark">
-              
-              <div class="d-md-flex justify-content-md-end">
-                <button class="btn btn-secondary btn-sm" type="button">Report</button>
-              </div> </div>
-          </div>   
-              
-          <div>
-            <div class="card border-dark mb-3 " style="max-width: 50rem;">
-              
-              <div class="card-body text-dark">
-                <div class="received-chats">
-                  <div class="received-chats-img">
-                      <img src="imgs/profile.png" style="display: inline-block;">
-
-                  </div>
-                  <div class="received-msg" style="   display: inline-block;
-                  padding:0 0 0 10px;
-                  vertical-align: top;
-                  width: 92%;
-              ">
-                      <div class="received-mgs-inbox" style="background: #ced4da none repeat scroll 0 0;  border-radius: 10px; color: #646464; font-size: 14px; margin:0; padding: 5px 10px 5px 12px; width: 70%;">
-                          <p>
-                      HALOOOOOOOOOOOO
-                          </p>
-                         
-                          
-                      </div>
-
-                  </div>
-                  <div class="outgoing-chats" style="overflow: hidden; margin: 26px 20px;">
-                
-                    <div class="outgoing-chat-msg"style="   width: 57%; float:left; margin-left:33%;">
-                        <div class="outgoing-mgs-inbox" style="background: #ced4da none repeat scroll 0 0;  border-radius: 10px; color: #646464; font-size: 14px; margin:0; padding: 5px 10px 5px 12px; width: 100%;">
-                            <p>
-                        HALOOOOOOOOOOOO
-                            </p>
-                            </div>
-                            </div>
-                            <div class="outgoing-chats-img " style="display: inline-block;">
-                              <img src="imgs/profile.png" style="display: inline-block;">
-                              </div>
-              </div>
-              </div>
-              
-               
-              </div>
-              
-              <div class="card-footer bg-transparent border-dark">
-                
-                <div class="d-md-flex justify-content-md-end">
-                  <button class="btn btn-secondary" type="button">Report</button>
-                </div> </div>
-            </div>  
-       
-        
-         
-        
-      </div>
-    </div>
-    </div>
-  </div>
-        
-      </div>
-    </div>
-    </div>
-  </div>
-  
-        
-      </div>
-    </div>
-    </div>
-  </div>
-
-</div> 
-<?php
-    
-  }
-  else
-  {
-    ?>
-      <h1 class="lead text-center text-dark mt-1 ms-5 mb-5 fw-normal">User doesn't exists.<br>
-    <?php
-  }
- ?>
+<div class="d-flex justify-content-center align-items-center h-100 ms-5 me-5">
+<div class="text-white">
+  <div class="d-flex">
+  <a href="#"><img src="imgs/profile.png" class=" ms-5 "style="border: 5px solid #000;"  ></a></div>
+  <h1 class="lead text-center text-dark mt-1 ms-5 mb-5 fw-normal">
+    Name<br >
+   <a class="text-secondary  " style="text-decoration: none;"> Username </a>
+  </h1>
 </div>
-
-
-
-    </section>
-<style>
   
+</div>
+</div>
+</div>
+</div>
+</div>
+    </section>
+  </main>
+  <!--End of IMAGE USER-->
+  <!--Public Notebook and Discussion-->
+   <div class="container" >
+      <div class="row">
+<!--Public Notebook-->
+        <div class="col-md-6 col-lg-7 mt-5 " >
+          <div class="card bg-light " style="min-height: 400px;">
+            
+            <nav class="navbar navbar-light justify-content-center sticky-top" style="background-color: #495057;"> <p class=" justify-content-center align-items-center mt-2" style=" color: #eee;"> Public Notebook</p> </nav>
+            <div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-offset="0" class="scrollspy-example" tabindex="0">
+            <div class="card-body text-center" style="height: auto;">
+
+              <div>
+                <div class="card border-dark mb-3 " style="max-width: 50rem;">
+                  <nav class="navbar navbar-light"> <ul class="nav nav-pills ms-2"  >
+                    <li class="nav-item  ">
+                      <p class="nav-link " href="#" style="color:black;">Date</p>
+                    </li> </nav>
+                  <div class="card-body text-dark">
+                    
+                    <p class="card-text1">Some quick example text to build on the card title and make up the bulk of the card's content .Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  </div>
+                  <div class="card-footer bg-transparent border-dark">
+                    
+                    <div>
+                  <button class="btn btn-none btn-sm float-start"> <i class="bi bi-star h4"></i></button>
+                      <button class="btn btn-secondary btn-sm float-end mt-1" type="button">Report</button>
+                    </div> </div>
+                </div>   
+                
+                  
+            
+         </div>
+         </div>
+        </div>
+          </div>
+        </div> 
+        <!-- End of Public Notebook-->
+        <!--- Discussion ---> 
+         <div class="col-md-6 col-lg-5 mt-5" >
+          <div class="card bg-light" style="min-height: 400px;">
+            
+            <nav class="navbar navbar-light justify-content-center sticky-top" style="background-color: #495057;"> <p class=" justify-content-center align-items-center mt-2" style=" color: #eee;"> Discussion</p> </nav>
+            <div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-offset="0" class="scrollspy-example" tabindex="0">
+            <div class="card-body text-center" style="height: auto;">
+             
+              <div>
+                <div class="card border-dark " style="max-width: 40rem;">
+                  
+                  <div class="card-body text-dark">
+                    <div class="received-chats">
+                      <div class="received-chats-img ">
+                          <img src="imgs/profile.png" style="display: inline-block;">
+  
+                      </div>
+                      <div class="received-msg " style="   display: inline-block;
+                      padding:0 0 0 10px;
+                      vertical-align: top;
+                      width: 92%;
+                  ">
+                          <div class="received-mgs-inbox" style="background: #ced4da none repeat scroll 0 0;  border-radius: 10px; color: #646464; font-size: 14px; margin:0; padding: 5px 10px 5px 12px; width: 70%;">
+                              <p>
+                          HALOOOOOOOOOOOO
+                              </p>
+                             
+                              
+                          </div>
+  
+                      </div>
+                      <div class="outgoing-chats" style="overflow: hidden; margin: 26px 20px; margin-left: 5px;">
+                    
+                        <div class="outgoing-chat-msg"style="   width: 57%; float:left; margin-left:33%;">
+                            <div class="outgoing-mgs-inbox" style="background: #ced4da none repeat scroll 0 0;  border-radius: 10px; color: #646464; font-size: 14px; margin:0; padding: 5px 10px 5px 12px; width: 100%;">
+                                <p>
+                            HALOOOOOOOOOOOO
+                                </p>
+                                </div>
+                                </div>
+                                <div class="outgoing-chats-img " style="display: inline-block;">
+                                  <img src="imgs/profile.png" style="display: inline-block;">
+                                  </div>
+                  </div>
+                  </div>                                  
+                  </div>
+                  
+                  <div class="card-footer bg-transparent border-dark">
+                    
+                    <div class="d-md-flex justify-content-md-end">
+                      <button class="btn btn-secondary btn-sm" type="button">Report</button>
+                    </div> </div>
+                </div>                                                                                            
+                    </div>
+                    </div>                                        
+                    </div>                                                                                                   
+          </div>
+          </div>
+        </div>
+
+    </div> 
+    <!-- End of Disscussion-->
+ 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!-- MDB -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <!-- Custom scripts -->
+    <script type="text/javascript"></script>
+  </body>
+</html>
+<style>
+  .body{
+    padding-bottom: 100px;
+}
 
 
 
 .scrollspy-example{
-    height: 300px;
+    height: 350px;
     overflow-y: scroll;
     overflow-x:hidden;
 
@@ -333,35 +224,19 @@ width: auto;"
     height: 5px;
 }
 .card-text{
-    min-height: 200px;
-}
-.tag{
-    width: 100px;
-    height: 100px;
-    background: rgb(131, 126, 122);
-    position: relative;
-    top: 500px;
-    left: 800px;
-    border-radius: 50%;
+    min-height: 300px;
+    min-width: auto;
 }
 
-.title{
-   
-   color: #fff;
-    margin: 5px;
-}
 
-.items{
-    background-color: #fff;
-    border: 1px solid #d1d1d1;
-    margin: 10px;
-    padding: 5px;
-}
+
+
 
 img
 {
     max-width: 100%;
-    border-radius: 50%;
+    border-radius: 60%;
+ 
 }
 
 .active h4{

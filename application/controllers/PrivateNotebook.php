1d05f152d6bfed7f0751d5b22747111702ec4211
Toffielicious <christophercamanag@gmail.com>
2021-11-29 22:56:40 +0800

    ahhh

--- application/controllers/PrivateNotebook.php
+++ application/controllers/PrivateNotebook.php
@@ -7,16 +7,45 @@
         public function __construct()
         {
             parent::__construct(); 
+            $this->load->model('PrivateNB_model');
         }
 
         public function index() {
             $data['navbar'] = 'main';
             $this->sitelayout->loadTemplate('pages/privatenotebook/viewprivatenotebook', $data); 
+            $action = $this->input->post('action');
+            if($action == 'Update')
+            {
+                $this->updatePrivateNotebook();
+            }
         }
 
-        public function updatePrivateNotebook() {
+        public function updatePrivateNotebook()
+        {
             $data['navbar'] = 'main';
             $this->sitelayout->loadTemplate('pages/privatenotebook/updateprivatenotebook', $data); 
+            
+            
+                $id = $this->session->userdata('user_ID');
+                $pageTimer = $this->input->post('appt');
+                $response = $this->PrivateNB_model->updateTimer($pageTimer, $id);
+    
+                if($response)
+                {
+                    echo 'kahitano';
+                }
+    
+                else
+                {
+                    echo 'garaba';
+                }
+            
+        }
+
+        public function updateTimer()
+        {
+            
+           
         }
     }
 

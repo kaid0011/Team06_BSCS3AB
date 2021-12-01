7aa3b80845deb6405ae5a0087964a993eb86480e
Allana <romero.allana2628@gmail.com>
2021-12-01 18:10:05 +0800

    forgot password (12-1)

---
+++ application/controllers/ForgotPassword.php
@@ -0,0 +1,110 @@
+<?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
+    
+    class ForgotPassword extends CI_Controller
+    {
+        public function __construct()
+        {
+            parent::__construct();
+            
+            $this->load->library('form_validation');
+            $this->load->model('ForgotPassword_model');
+        }
+
+        public function index() 
+        {
+            $data['navbar'] = 'login';
+            $this->sitelayout->loadTemplate('pages/authentication/forgotpassword', $data); 
+        }
+
+        public function validation()
+        {
+            $action = $this->input->post('action');
+
+            if($action == 'Confirm')
+            {
+                $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+
+                if($this->form_validation->run())
+                {
+                    $email = $this->input->post('email');
+
+                    $result = $this->ForgotPassword_model->checkEmail($email);
+                    if($result)
+                    {
+                        $code = random_string('numeric', 6);
+
+                        $userdata = array(
+                            'email' => $email,
+                            'code'  =>  $code
+                        );
+                        $this->session->set_userdata($userdata);
+                        $this->sendEmail();
+                    }
+                    else
+                    {
+                        echo 'This Email is not registered.'; 
+                        #will change laturrrrr
+                    }
+                }
+                else
+                {
+                    $data['navbar'] = 'login';
+                    $this->sitelayout->loadTemplate('pages/authentication/forgotpassword', $data); 
+                }
+            }
+            else    #if ($action == 'Back')
+            {
+                $data['navbar'] = 'login';
+                $this->sitelayout->loadTemplate('pages/authentication/login', $data); 
+            }
+        }
+
+        public function sendEmail()
+        {
+            $email = $this->session->userdata('email');
+            $code = $this->session->userdata('code');
+
+            $subject = "Forgot Password";
+            $message = "
+            <h3>Hello!</h3>
+            <p>Here is your verification code.</p><br>
+            <h4>$code</h4>
+            ";      #will change laturrr
+
+            $config = array(
+                'protocol'  => 'smtp',
+                'smtp_host' => 'ssl://smtp.googlemail.com',
+                'smtp_port' => 465,
+                'smtp_user' => 'Team6.VirtualDiary2022@gmail.com',
+                'smtp_pass' => 'team6@3ab',
+                'mailtype'  => 'html', 
+                'charset'   => 'iso-8859-1'
+            );
+            
+            $this->load->library('email');
+            $this->email->initialize($config);
+
+            $this->email->set_newline("\r\n");
+            
+            $this->email->from('Team6.VirtualDiary2022@gmail.com', 'Virtual Diary');
+            $this->email->to($email);
+
+            $this->email->subject($subject);
+            $this->email->message($message);
+
+            $send = $this->email->send();
+
+            if($send)
+            {
+                $data['navbar'] = 'login';
+                $this->sitelayout->loadTemplate('pages/authentication/confirmation', $data);
+            }
+            else
+            {
+                echo "Error";
+                #will change laturrr
+            }
+        }
+    }
+?>
--- application/views/pages/authentication/changepassword.php
+++ application/views/pages/authentication/changepassword.php
@@ -2,26 +2,28 @@
     <div class="container">
         <div class="row align-items-center justify-content-between">
             <div class="col-md p-5 ptt-9">
-                <img src="assets/images/login/pic4.png" class="img-fluid" alt="">
+                <img src="<?= base_url('assets/images/login/pic4.png')?>" class="img-fluid" alt="">
             </div>
             <div class="col-md p-5">
                 <h1 class="h1 mb-4 pt-4 text-dark">Change Password</h1>
                 <p class="lead">
-                    <form>
+                    <form method="post" action="<?= base_url('changepassword/validation') ?>" autocomplete="off">
                         <div class="mb-5">
                             <!-- New Password -->
                             <label for="exampleInewPassword1" class="form-label"> New Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1">
+                            <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+                            <span class="text-danger"><?php echo form_error('password'); ?></span>
                             <!-- Confirm Password -->
                             <div class="mb-5">
                                 <label for="exampleInewPassword1" class="form-label"> Confirm Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1">
+                                <input type="password" name="confirm_password" class="form-control" id="exampleInputPassword1">
+                                <span class="text-danger"><?php echo form_error('confirm_password'); ?></span>
                             </div>
                             
                             <!-- Confirm -->
-                            <button type="button" style="background-color: #f0b63a;" class="btn-lg border border-3 border-dark me-2 "><a style="text-decoration: none; color: #212529;" href="<?php echo base_url(); ?>login"> Confirm</a></button>
-                            <!-- Password -->
-                            <button type="button" style="background-color: #f0b63a;" class="btn-lg border border-3 border-dark"><a style="text-decoration: none; color: #212529;" href="<?php echo base_url(); ?>confirmation"> Back</a></button>
+                            <input type="submit" value="Confirm" name ="action" style="background-color: #f0b63a;" class="btn-lg border border-3 border-dark me-2 "><a style="text-decoration: none; color: #212529;">
+                            <!-- Back -->
+                            <input type="submit" value="Back" name ="action" style="background-color: #f0b63a;" class="btn-lg border border-3 border-dark"><a style="text-decoration: none; color: #212529;">
 
                     </form>
                 </p>

65689665a4a73b08d9068c591b219152c1ee261c
Allana <romero.allana2628@gmail.com>
2021-12-13 23:49:00 +0800

    Complete Support Team Access Account (12-13)

--- application/controllers/SupportTeam.php
+++ application/controllers/SupportTeam.php
@@ -7,7 +7,7 @@
         {
             parent::__construct();
             
-            //$this->load->model('SupportTeam_model');
+            $this->load->model('SupportTeam_model');
         }
 
         public function index()
@@ -15,5 +15,58 @@
             $data['navbar'] = 'main';
             $this->sitelayout->loadTemplate('pages/supportteam/support', $data);
         }
+
+        public function reportUserWall()
+        {
+            $data['viewAllReports'] = $this->SupportTeam_model->viewAllReports();
+
+            $data['navbar'] = 'main';
+            $this->sitelayout->loadTemplate('pages/supportteam/reportuserwall', $data);
+        }
+
+        public function updateIndivReport()
+        {
+            $action = $this->input->post('action');
+            $report_ID = $this->input->post('report_ID');
+            $reportedUser_ID = $this->input->post('reportedUser_ID');
+
+            if($action == 'View Report')
+            {
+                $data['viewIndivReport'] = $this->SupportTeam_model->viewIndivReport($report_ID);
+                $data['getReportedUserData'] = $this->SupportTeam_model->getReportedUserData($reportedUser_ID);
+                $data['viewAllReports'] = $this->SupportTeam_model->viewAllReports();
+
+                $data['navbar'] = 'main';
+                $this->sitelayout->loadTemplate('pages/supportteam/updateindivreport', $data);
+            }
+        }
+
+        public function updateReport()
+        {
+            $action = $this->input->post('action');
+            $report_ID = $this->input->post('report_ID');
+            $reportedUser_ID = $this->input->post('reportedUser_ID');
+            $userStatus = $this->input->post('userStatus');
+            $reportStatus = $this->input->post('reportStatus');
+            $comment = $this->input->post('comment');
+            
+            if($action == 'Update')
+            {
+                $response = $this->SupportTeam_model->updateReport($report_ID, $reportedUser_ID, $userStatus, $reportStatus, $comment);
+                if($response)
+                {
+                    $this->reportUserWall();
+                }
+            }
+            else if($action == 'Delete')
+            {
+                $response = $this->SupportTeam_model->deleteReport($report_ID);
+                if($response)
+                {
+                    $this->reportUserWall();
+                }
+            }
+        }
+
     }
 ?>

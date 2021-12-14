5f77e6005ae9410bf618fa202e33f159ab2e43ca
zek <zekguevarra1053@gmail.com>
2021-12-14 21:53:04 +0800

    updateprofile responsive

--- application/views/pages/navbar/updateprofile.php
+++ application/views/pages/navbar/updateprofile.php
@@ -12,25 +12,25 @@ if (isset($_POST["insert"])) {
 
 <section style="background-color: #e9ecef; min-height: 75vh; padding-top: 80px">
   
-    <h1 class="h1 mb-4 pt-1 text-dark text-center">Update Profile</h1>
+    <h1 class="h1 mb-4 pt-1 text-dark text-center ps-5">Update Profile</h1>
     <!--div class="p-5 text-center bg-image" style="background-image: url('imgs/white.png'); height: 300px; width: auto;"-->
-<div class="container d-flex flex-column justify-content-center align-items-center ">
+<div class="container  justify-content-center align-items-center ">
 
-    <div class="d-flex align-items-center justify-content-center">
+    <div class="align-items-center justify-content-center">
 
-      <div>
+      <div class="justify-content-center align-items-center text-center ps-5">
         <?php
         $user_ID = $this->session->userdata('user_ID');
         $query = "SELECT * FROM user WHERE user_ID = $user_ID";
         $result = mysqli_query($connect, $query);
         while ($row = mysqli_fetch_array($result)) {
-          echo '<img style="width: 200px; height: 200px; border-radius: 100px;" src="data:image/jpeg;base64, ' . base64_encode($row['user_displayImage']) . '"';
+          echo '<img style="width: 200px; height: 200px; border-radius: 100px; justify-items:center;" src="data:image/jpeg;base64, ' . base64_encode($row['user_displayImage']) . '"';
         }
         ?>
       </div>
       
       <form method="post" enctype="multipart/form-data">
-        <div class="d-flex justify-content-center pt-3 ">
+        <div class="align-items-center text-center justify-content-center pt-3 ps-5">
           <input type="file" name="image" id="image" />
         </div>
         <!-- <a href="#"><img src="<?= base_url('assets/images/visitedprofile/profile.png') ?>" class="rounded-circle border border-3 border-secondary mb-2" alt="..."></a>  -->
@@ -46,20 +46,21 @@ if (isset($_POST["insert"])) {
     <div class="col-md p-5 d-flex justify-content-center align-items-center">
       <p class="lead align-center">
 
-      <form method="post" action="<?= base_url('updateprofile/updateusername') ?>" autocomplete="off">
-        <!-- Username -->
         <div class="mb-3 text-start text-secondary inputbox">
-          <label for="exampleInputlastname1" class="form-label ps-2 mt-2">Username</label>
-          <div class="input-group input-group-lg">
-            <input type="text" class="form-control mb-1" style="font-size: 15px;" id="examplelastname1" name="userName" aria-describedby="emailHelp" value="<?php echo $this->session->userdata('userName'); ?>" />
-            <!-- Resend Button -->
-            <input type="submit" value="Update" name="action" class="input-group-text" style="background-color:#ced4da; font-size: 15px; Height: 41px;" id="basic-addon2">
-          </div>
-          <span class="text-danger"><?php echo form_error('userName'); ?></span>
+    
+
+      <form method="post" class="responsiveInput" action="<?= base_url('updateprofile/updateusername') ?>" autocomplete="off">
+        <!-- UserName -->
+        <label for="exampleInputname1" class="form-label ps-2 mt-2">Username</label>
+        <div class="input-group input-group-lg">
+          <input type="text" class="form-control mb-1" style="font-size: 15px;" id="examplelastname1" name="userName" aria-describedby="emailHelp" value="<?php echo $this->session->userdata('userName'); ?>" />
+          <!-- Resend Button -->
+          <input type="submit" value="Update" name="action" class="input-group-text" style="background-color:#ced4da; font-size: 15px; Height: 41px;" id="basic-addon2">
+        </div>
+        <span class="text-danger"><?php echo form_error('displayName'); ?></span>
       </form>
 
-
-      <form method="post" action="<?= base_url('updateprofile/updatedisplayname') ?>" autocomplete="off">
+      <form method="post" class="responsiveInput" action="<?= base_url('updateprofile/updatedisplayname') ?>" autocomplete="off">
         <!-- Display Name -->
         <label for="examplefirstname1" class="form-label ps-2 mt-2">Display Name</label>
         <div class="input-group input-group-lg">
@@ -71,7 +72,7 @@ if (isset($_POST["insert"])) {
       </form>
 
 
-      <form method="post" action="<?= base_url('updateprofile/updateemail') ?>" autocomplete="off">
+      <form method="post" class="responsiveInput" action="<?= base_url('updateprofile/updateemail') ?>" autocomplete="off">
         <!-- E-mail Address -->
         <label for="exampleInputEmail1" class="form-label ps-2 mt-2">E-mail Address</label>
         <div class="input-group input-group-lg">
@@ -83,7 +84,7 @@ if (isset($_POST["insert"])) {
       </form>
 
       <form method="post" action="<?= base_url('updateprofile/updatedprofile') ?>" autocomplete="off">
-        <div class="col-md d-flex justify-content-start align-items-center">
+        <div class="col d-flex justify-content-start align-items-center">
           <!-- Change Password Button -->
           <input type="submit" name="action" value="Change Password" class="btn btn-sm text-secondary mt-1" style="text-decoration: underline; margin-top: -15px;"></input>
         </div>
@@ -98,6 +99,17 @@ if (isset($_POST["insert"])) {
     @media (min-width: 768px) {
       .inputbox {
         width: 600px;
+      }
+    }
+    @media (max-width: 425px) {
+      section{
+        width:100%;
+        margin:0;
+        padding:0;
+        overflow-x:hidden;
+      }
+      .responsiveInput{
+        padding-left:20px;
       }
     }
   </style>

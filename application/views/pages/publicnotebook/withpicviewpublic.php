a356a17e768b1da8fd4df10cb7b3aa8429f676f9
zek <zekguevarra1053@gmail.com>
2022-01-14 23:17:54 +0800

    button pakilagay saleft

--- application/views/pages/publicnotebook/withpicviewpublic.php
+++ application/views/pages/publicnotebook/withpicviewpublic.php
@@ -1,18 +1,15 @@
   <div class="row ms-5">
     
-    <div class="col ms-5 d-flex justify-content-center">
+    <div class="col ms-5 d-flex justify-content-between py-3">
+    <button type="submit" style="background-color: #f0b63a; justify-content-start" class="btn border border-2 border-dark">Back</button>
       <!-- New Page Button -->
       <a href= "<?= base_url('publicnotebook/createpublicnotebook')?>">
         <button class="ms-5 m-3 btn border border-secondary border-3" style="border-radius: 100px;"><h2>+</h2></button>
       </a>
-    </div>
-    <button type="submit" style="background-color: #f0b63a;" class="btn border border-2 border-dark">Back</button>
-    <div class="col-2 justify-content-end pe-5 pt-3">    
-      <!-- Timestamp -->
       <form action="<?= base_url('publicnotebook/searchpage') ?>" method="post" class="form d-flex">
         <!--changes-->
           <input type="date" name="date" class="form-control me-2">
-          <button type="submit" style="background-color: #f0b63a;" class="btn border border-2 border-dark">Search</button>
+          <button type="submit" style="background-color: #f0b63a;" class="btn border border-2 border-dark me-3">Search</button>
       </form>
     </div>
   </div>

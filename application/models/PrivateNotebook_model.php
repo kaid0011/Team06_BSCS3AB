3fa6c1b9acd57dabf9ad6f16cc26180e56c72b4a
Wana <jmestrella42601@gmail.com>
2022-02-04 21:37:45 +0800

    Automatic Reset Private Notebook Delete Image (2-4)

--- application/models/PrivateNotebook_model.php
+++ application/models/PrivateNotebook_model.php
@@ -63,10 +63,59 @@ class PrivateNotebook_model extends CI_Model
         }
     }
 
+    public function removeImage($id)
+        {
+            $filename = $id."_privateNotebookImage";
+            $extension = ".jpg";
+            $path_filename_ext = APPPATH.'/uploads/privatenotebook/'.$filename.$extension;
+
+            if(file_exists($path_filename_ext))
+            {
+                $extension = ".jpg";
+                unlink($path_filename_ext);
+            }
+            else
+            {
+                $extension = ".jpeg";
+                $path_filename_ext = APPPATH.'/uploads/privatenotebook/'.$filename.$extension;
+                if(file_exists($path_filename_ext))
+                {
+                            
+                    $extension = ".jpeg";
+                    unlink($path_filename_ext);
+                }
+                else
+                {
+                    $extension = ".png";
+                    $path_filename_ext = APPPATH.'/uploads/privatenotebook/'.$filename.$extension;
+                    
+                    if(file_exists($path_filename_ext))
+                    {
+                        $extension = ".png";
+                        unlink($path_filename_ext);
+                        
+                    }
+                }
+            }
+        }
+
     public function resetPage()
     {
         date_default_timezone_set("Asia/Manila");
         $currentTime = date("H:i s");
+
+        $start = "00:00:00";
+
+        $this->db->where('pageTimer >=', $start);
+        $this->db->where('pageTimer <=', $currentTime);
+        $this->db->select('privateNB_ID');
+        $query = $this->db->get("privatenb_pages");
+        foreach ($query->result() as $row) 
+        {   
+            $id = $row->privateNB_ID;   
+            $this->removeImage($id);
+            echo $id;
+        }
 
         $reset = "UPDATE privatenb_pages SET pageInput = 'This is my private page.', pageTheme = 'Light', pageTimer = '00:00:00' WHERE pageTimer BETWEEN '00:00:00' AND '$currentTime'";
         $this->db->query($reset);

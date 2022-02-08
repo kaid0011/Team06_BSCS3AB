db21b06080d48aaa969dc0c8161178237c7a909a
Kim <kimdiannebagasona@gmail.com>
2022-02-08 19:12:23 +0800

    admin acc updates (02-08)

--- application/views/pages/supportteam/recommendation.php
+++ application/views/pages/supportteam/recommendation.php
@@ -1,10 +1,10 @@
-<section>
-        <img src="<?=base_url('assets/images/registration/mainte.png')?>"style="height: 300px; width: 400px;" alt="maintenance image">
-        <h2>Under maintenance</h2>
+<section style="background-color: #e9ecef; min-height: 75vh;">
+        <img src="<?=base_url('assets/images/registration/mainte.png')?>"style="height: 300px; width: 400px; border-radius:100px;" alt="maintenance image">
+        <h2 class="pt-5">Under maintenance</h2>
         <p class="p-4 px-5"style=" font-size: 24px;padding-bottom: 20px;"> 
         Sorry for the inconvenience but we’re performing some maintenance at the moment.
             <br>we’ll be back online shortly!</p>
-        <a href="<?= base_url('supportteam') ?>"><input type="submit" value="Go Back" name="action" style="background-color: #f0b63a; width: 75px;" class="p-2 mb-5 btn-sm border border-2 border-dark btn-end"></a>
+        <a href="<?= base_url('supportteam') ?>"><input type="submit" value="Go Back" name="action" style="background-color: #f0b63a;" class="btn-lg border border-2 border-dark"></a>
       </section>
 <style>
     body{
